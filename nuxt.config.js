a19e9196550eea4d64061f83d9fc9f1d6131a473
Artem Byastik <stalker3343@gmail.com>
2022-03-29 21:20:15 +0300

    add comand for start on localhost

--- nuxt.config.js
+++ nuxt.config.js
@@ -45,10 +45,10 @@ export default {
     // Workaround to avoid enforcing hard-coded localhost:3000: https://github.com/nuxt-community/axios-module/issues/308
     baseURL: '/',
   },
-  server: {
-    host: '0', // default: localhost
-    port: 3000 // default: 3000
-  },
+  // server: {
+  //   host: '0', // default: localhost
+  //   port: 3000 // default: 3000
+  // },
 
   // Vuetify module configuration: https://go.nuxtjs.dev/config-vuetify
   vuetify: {
